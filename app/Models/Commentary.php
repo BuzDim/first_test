c467bbe56ec5edaee92e78228176d3afc977fbd2
BuzDim <buzykin@bk.ru>
2024-02-06 17:01:31 +0300

    модель для комментариев

---
+++ app/Models/Commentary.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\PostController;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Commentary extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(PostController::class);
+    }
+}
